8ceaca1e5245eee971d3ea876b3040a12b78ff8b
Kishan Suresh <kishansureshin@gmail.com>
2021-01-18 22:25:56 +0000

    added feature to add the items

---
+++ resources/js/components/ItemsComponent.vue
@@ -0,0 +1,156 @@
+<template>
+    <div class="col-sm-12">
+        <div v-if="loading">loading ...</div>
+        <div class="row mb-1">
+            <div class="col-md-12"> 
+                <form v-on:submit.prevent="createItem()" accept-charset="UTF-8" method="get">
+                    <div class="input-group"> 
+                        <input type="text" v-model="item.name" placeholder="Add an item name" v-bind:id="item" v-bind:name="item"  class="form-control">
+                        
+                        <span class="input-group-btn">
+                            <input type="submit"  class="btn btn-info ml-1" value="Save">
+                        </span>
+                    </div>
+                    <span class="text-danger" role="alert">
+                        <strong>{{ errors.get('name') }}</strong>
+                    </span>
+                </form>
+            </div>
+        </div>
+        <!-- draggable component used for sorting the elements -->
+        <draggable :list="items" :options="{animation:200}" :element="'div'" @change="saveSortItems(items)">
+            <div class="" v-for="item in sortItems(items)" v-bind:key="item.id">
+                <div class="card-body card mb-1" >
+                    <div style="position: relative">
+                        <i @click="deleteItem(item.id)" class="fa fa-trash delete-button"></i>
+                    </div>
+                    <div class="big-checkbox">
+                        <input :value="item.id" class="form-check-input ml-1" type="checkbox"  v-bind:id="'item-check'+item.id" v-model="itemsBought"  @change="boughtItem(item.id,$event)">
+                    </div>
+                    <div class="ml-5">
+                        <h4 v-if="itemsBought.includes(item.id)"><strike>{{ item.name }}</strike></h4>
+                        <h4 v-else >{{ item.name }}</h4>
+                    </div> 
+                </div>
+            </div>
+        </draggable>
+    </div>
+</template>
+
+<script>
+/**
+ * this class is ussed for catching validation errors and displaying error message
+ */
+class Errors {
+    constructor() {
+        this.errors = {};
+    }
+
+    get(field) {
+        if( this.errors[field]) {
+            return this.errors[field][0];
+        }
+    }
+
+    record(errors) {
+        this.errors = errors.errors;
+    }
+}
+
+// for drag and drop feature
+import draggable from 'vuedraggable';
+
+export default {
+
+    components : {
+        draggable
+    },
+    
+    data() {
+        return {
+            loading : false,
+            errors : new Errors(),
+            items : [],
+            item : {
+                id : '',
+                name : '',
+                price : '',
+                sort : '',
+                bought : ''
+            },
+            itemsBought: [],
+        }
+    },
+
+    methods: {
+
+       
+        
+        /**
+         * method for creating an item
+         */
+        createItem() {
+
+            var maxOrder = Math.max.apply(Math, this.items.map(function(o) { return o.sort; }));
+            
+            axios({
+                method: 'post',
+                url: '/api/items',
+                data: {
+                    item_id : this.item.id,
+                    name : this.item.name,
+                    bought : 0,
+                    sort : maxOrder + 1 
+                }
+            })
+            .then(res => {
+                this.errors = new Errors();
+                this.$emit("item-added");
+                this.item = {};
+                Vue.use(VueToast);
+                let instance = Vue.$toast;
+                instance.success('Item created!', {
+                    position: 'top-right'
+                });
+            })
+            .catch((error) => {
+                this.errors.record(error.response.data);
+            });
+        },
+        /**
+         * method for updating the bought status of the item
+         * @param item_id
+         * @param event
+         */
+        boughtItem(item_id, event) {
+            
+            if (event.target.checked) {
+                var checked = 1;
+            }
+            else {
+                var checked = 0;
+            }
+
+            axios({
+                method: 'put',
+                url: '/api/items/bought',
+                data: {
+                    item_id : item_id,
+                    bought : checked
+                }
+            })
+            .then(res => {
+                Vue.use(VueToast);
+                let instance = Vue.$toast;
+                instance.success('Item updated!', {
+                    position: 'top-right'
+                });
+            })
+            .catch(error =>{
+                this.errors.record(error.response.data);
+            });
+        }
+
+    }
+}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,20 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-10">
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
                 </div>
-            @endif
-            <home-component />
+            </div>
         </div>
     </div>
 </div>
